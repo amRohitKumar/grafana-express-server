77a0030c7bb7e92b9a00b14c3e5cbc2a3b46feab
Rohit Kumar <shutupRohit@gmail.com>
2025-04-08 03:19:31 +0530

    update metrics

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const app = express();
 const port = 8080;
 
 const collectDefaultMetrics = promClient.collectDefaultMetrics;
-const Registry = promClient.Registry;
-const register = new Registry();
-collectDefaultMetrics({ register });
+collectDefaultMetrics({ register: promClient.register });
 
 app.get("/", (req, res) => {
   return res.json({ message: "Server is helathy!" });
@@ -26,14 +24,14 @@ app.get("/slow", async (req, res) => {
 
 app.get("/metrics", async (req, res) => {
   try {
-    const metrics = await register.metrics();
-    res.set("Content-Type", register.contentType);
+    res.set("Content-Type", promClient.register.contentType);
+    const metrics = await promClient.register.metrics();
     res.send(metrics);
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
   }
-}); 
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
