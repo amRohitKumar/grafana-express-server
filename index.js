fce99fc6906f7425f81d7ce9ed92f16e6f4e64df
Rohit Kumar <shutupRohit@gmail.com>
2025-04-08 02:53:17 +0530

    add prometheus client

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const promClient = require("prom-client");
 const { heavyTask } = require("./utils.js");
 
 const app = express();
 const port = 8080;
+
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+const Registry = promClient.Registry;
+const register = new Registry();
+collectDefaultMetrics({ register });
 
 app.get("/", (req, res) => {
   return res.json({ message: "Server is helathy!" });
@@ -17,6 +23,17 @@ app.get("/slow", async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
+
+app.get("/metrics", async (req, res) => {
+  try {
+    const metrics = await register.metrics();
+    res.set("Content-Type", register.contentType);
+    res.send(metrics);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+}); 
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
